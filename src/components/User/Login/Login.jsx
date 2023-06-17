fad6a751cce8bbe4a0ed4e510c957b2aa803609d
Gregório Magno Neto <gregorio.neto@maida.health>
2023-06-17 10:32:10 -0300

    modificando user fake

--- src/components/User/Login/Login.jsx
+++ src/components/User/Login/Login.jsx
@@ -12,7 +12,7 @@ function initialState() {
 
 function login({ email, password }) {
   // console.log("login");
-  if (email === "gregorio@gmail.com" && password === "gregorio") {
+  if (email === "Vanusa@gmail.com" && password === "vanusa") {
     // console.log("senha e email ok");
     return { token: "123", erro: "" };
   } else {
